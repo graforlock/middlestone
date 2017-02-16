ba15132b8e8ba34762e4ff6e8557488cad2bf906
Maciej Sitko <maciej@neatcube.co.uk>
2017-02-16 23:39:25 +0000

    compose is now async/promise compatible

--- src/lib/compose.js
+++ src/lib/compose.js
@@ -1,3 +1,11 @@
-export default function compose(...fns) {
-    return (v) => fns.reverse().reduce((x, y) => y(x), v);
+import isThennable from './is-thennable';
+
+function fromThen(x, fn) {
+    return x.then(fn);
+}
+
+export default function asyncCompose(...fns) {
+    return (v) => fns.reverse().reduce((x, y) => {
+        return isThennable(x) ? fromThen(x, y) : y(x);
+    }, v);
 }
