bd4946bb856636369043efb0ae59157b607f026f
Maciej Sitko <maciej.sitko@livebookings.london>
2017-02-20 12:42:50 +0000

    Fixed a bug regarding incorrect error type assertion.

--- src/core/fetch.js
+++ src/core/fetch.js
@@ -1,22 +1,22 @@
 import {identity} from '../lib';
 
 export function getComposable(middleware) {
-    const composable = middleware.filter(x => typeof x === 'function');
-    return composable.length ? composable : identity;
+    const composables = middleware.filter(x => typeof x === 'function');
+    return composables.length ? composables : identity;
 }
 
 export function getConfig(middleware) {
-    const objects = middleware.filter(x => typeof x === 'object');
-    return objects[0] ? objects[0] : {};
+    const config = middleware.filter(x => typeof x === 'object');
+    return config.length ? config[0] : {};
 }
 
 const handleStatus = (config, x) => {
-    return x.isErr() && config[x.unwrap()] === 'undefined';
+    return x.isErr() && config[x.unwrap()];
 };
 
 export function toJson(composed, config) {
     return res =>
          handleStatus(config, res)
-            ? res.map(r => r.json().then(composed))
-            : res.orElse(config[res.unwrap()]);
+             ? res.orElse(config[res.unwrap()])
+             : res.map(r => r.json().then(composed))
 }
